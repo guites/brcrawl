aa3605ea7fc8d930b6a9df455d074095e06e9796
Guilherme Garcia <gui.garcia67@gmail.com>
2026-02-10 11:55:55 -0300

    scraper: save results to subdirectories

--- scraper/brcrawl.sh
+++ scraper/brcrawl.sh
@@ -1,24 +1,30 @@
 #!/bin/bash
 
-if [[ ! -f "urls.txt" || ! -f "blocklist.txt" ]]; then
+if [[ -z "$1" || ! -d "$1" ]]; then
+    echo "Usage: ./brcrawl.sh <directory>"
+    echo "<directory> is the path to a directory with a urls.txt and blocklist.txt file, and where the program output will be saved."
+    exit 1
+fi
+
+if [[ ! -f "$1/urls.txt" || ! -f "$1/blocklist.txt" ]]; then
     echo "Please create the urls.txt and blocklist.txt files"
     echo "See README.md for instructions"
     exit 1
 fi
 
 # 1. Gets all external URLs mentioned on existing blog posts
-uv run scrapy crawl external_urls -a urls_file=urls.txt -o external_urls.jsonl
+uv run scrapy crawl external_urls -a urls_file="$1/urls.txt" -o "$1/external_urls.jsonl"
 
 # 2. Flatten list of external urls and remove duplicate domains
-uv run unique_urls.py external_urls.jsonl > unique_urls.txt
+uv run unique_urls.py "$1/external_urls.jsonl" > "$1/unique_urls.txt"
 
 # TODO: how do we filter out mastodon instances?
 # TODO: use a separate list for filtering subdomains? ex. xxx.substack.com
 # 3. Filter out known unwanted domains and URLs
-uv run filter_urls.py unique_urls.txt blocklist.txt > filtered_urls.txt
+uv run filter_urls.py "$1/unique_urls.txt" "$1/blocklist.txt" > "$1/filtered_urls.txt"
 
 # 4. Get RSS links for the external URLs
-uv run scrapy crawl rss -a urls_file=filtered_urls.txt -o rss.jsonl
+uv run scrapy crawl rss -a urls_file="$1/filtered_urls.txt" -o "$1/rss.jsonl"
 
 # 5. Determine whether website is written in portuguese
 # TODO: https://pypi.org/project/langdetect or
