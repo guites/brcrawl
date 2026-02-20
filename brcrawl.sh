39cfc02eeeb81e59ff5e618e4a0d69bbf7a6e866
Guilherme Garcia <gui.garcia67@gmail.com>
2026-02-20 16:45:46 -0300

    add orchestration script

---
+++ brcrawl.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+set -e
+set -u
+
+# Orchestrates processes to crawl verified blogs and find
+# links to new blogs written in portuguese.
+# This is the glue between the backend/ and scraper/ subprojects.
+# The database at backend/ manages which blogs where crawled and when
+# and keeps a status for every blog (whether they have been verified,
+# just been crawled, blocked, etc);
+
+# Creates new run directory
+# adapted from https://stackoverflow.com/a/2793856
+function get_dir() {
+    local dir_name=$(cat /dev/urandom | tr -cd 'a-f0-9' | head -c 16)
+    local dir_path="$1/runs/$dir_name"
+    if [[ -d "$dir_path" ]]; then
+        echo $(get_dir "$1")
+    else
+        echo $dir_path
+    fi
+}
+
+ROOT_DIR="$PWD"
+BACKEND_DIR="$PWD/backend"
+SCRAPER_DIR="$PWD/scraper"
+
+DIR_PATH=$(get_dir "$ROOT_DIR")
+mkdir "$DIR_PATH"
+
+# Generates the seed URLs for crawling
+cd "$BACKEND_DIR"
+uv run flask crawl-feeds --limit 25 --output "$DIR_PATH/seeds.jsonl" --mark-crawled
+
+# Creates the blocklist, composed of known unwanted domains and all
+# domains we have already registered to avoid unnecessary http requests
+uv run flask known-domains --output "$DIR_PATH/blocklist.txt"
+
+# Starts the web crawling pipeline
+cd "$SCRAPER_DIR"
+./scrape.sh "$DIR_PATH"
+
+cd "$BACKEND_DIR"
+
+# Import domains that have no valid feed url to the database blocklist
+uv run flask import-blocklist "$DIR_PATH/no_rss.txt"
+
+# Import feeds that are not in portuguese as "blocked" to the database
+uv run flask import-feeds "$DIR_PATH/lang_detect_other.jsonl" --feed-status="blocked" --output "$DIR_PATH/import_feeds_lang_detect_other.json" --descr="lang_detect_other"
+
+# Import feeds that are **not** personal_blogs as "blocked" to the database
+uv run flask import-feeds "$DIR_PATH/llm_classifier_false.jsonl" --feed-status="blocked" --output "$DIR_PATH/import_feeds_llm_classifier_false.json" --descr="llm_classifier_false"
+
+# Import feeds considered personal_blogs as "crawled" to the database
+uv run flask import-feeds "$DIR_PATH/llm_classifier_true.jsonl" --feed-status="crawled" --output "$DIR_PATH/import_feeds_llm_classifier_true.json"
