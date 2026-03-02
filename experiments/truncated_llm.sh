f45805e06dcfb60ecdcb919a97ac3092f714ebfc
Guilherme Garcia <gui.garcia67@gmail.com>
2026-03-02 19:19:50 -0300

    experiments: wip truncated_llm scraping logic

--- experiments/truncated_llm.sh
+++ experiments/truncated_llm.sh
@@ -1,35 +1,73 @@
 #!/bin/bash
+# We are testing whether truncating the home page of websites being sent to the llm for zero shot classification has a meaningful impact on page classification (personal blog true/false).
+#
+# Since we have the classification results from sending the full page text saved to llm_classifier_true.jsonl 
 set -e
 set -u
 
-shopt -s globstar
+UV="/home/ubuntu/.local/bin/uv"
+EXPERIMENTS_DIR="$PWD"
+RUNS_DIR="$EXPERIMENTS_DIR/../runs"
+SCRAPER_DIR="$EXPERIMENTS_DIR/../scraper"
+declare -i PROCESSED_FILES=0
+PROCESSED_FILES_NAMES=()
 
-ROOT_DIR="$PWD"
-RUNS_DIR="$ROOT_DIR/runs"
-
-
-if [ "$#" -eq 0 ]; then
-    echo "Usage: ./llm_truncate.sh <page_size_limit>";
+if [ ! "$#" -eq 2 ]; then
+    echo "Usage: ./llm_truncate.sh <page_size_limit> <number_of_runs>";
+    echo "  <page_size_limit> is how many characters from the page we send to the LLM"
+    echo "  <number_of_runs> is how many llm_classifier.jsonl files we should process"
     exit 1
 fi
 
 TRUNCATE_SIZE="$1"
+NUMBER_RUNS="$2"
+
+
+if [[ "$EXPERIMENTS_DIR" != *"/experiments" ]]; then
+    echo "Run script from brcrawl/experiments directory."
+    exit 1
+fi
 
 echo "Truncating pages to $TRUNCATE_SIZE characters."
+echo "Will run $NUMBER_RUNS time(s)."
+
+shopt -s globstar
 
 for dir in "$RUNS_DIR"/**/; do
+    if [ "$PROCESSED_FILES" -gt "$NUMBER_RUNS" ]; then
+        echo "Finished running $NUMBER_RUNS times."
+        exit 0
+    fi
+    echo "---------------------------------------"
     echo "Entering directory: $dir"
     cd "$dir"
 
-    output_file_true="$dir/llm_classifier_true_truncate_$TRUNCATE_SIZE.jsonl"
-    output_file_false="$dir/llm_classifier_false_truncate_$TRUNCATE_SIZE.jsonl"
-    if [ ! -f "$output_file_true" ]; then
-        echo "File <$output_file_true> not found. Running spider..."
+    input_file="$dir/llm_classifier.jsonl"
+    output_file="$dir/llm_classifier_truncate_$TRUNCATE_SIZE.jsonl"
+
+    if [ ! -f "$input_file" ]; then
+        echo "Directory has no valid llm_classifier.jsonl file. Skipping..."
+        cd - > /dev/null # Go back to the previous directory
+        continue
     fi
-    if [ ! -f "$output_file_false" ]; then
-        echo "File <$output_file_false> not found. Running spider..."
+
+    if [ ! -f "$output_file" ]; then
+        echo "File <$output_file> not found. Running spider..."
+        cd "$SCRAPER_DIR"
+        "$UV" run scrapy crawl llm_classifier_truncate -a urls_file="$input_file" -a truncate_text="$TRUNCATE_SIZE" -o "$output_file"
+        cd - > /dev/null # Go back to the previous directory
+        PROCESSED_FILES=$((PROCESSED_FILES+1))
+        PROCESSED_FILES_NAMES+="$input_file"
+    else
+        echo "File <$output_file> already exists. Skipping..."
     fi
-    uv run scrapy crawl llm_classifier -a urls_file="$RUNS_DIR/llm_classifier_true.jsonl" -a truncate_text=1000 -o "$output_file_true"
-    uv run scrapy crawl llm_classifier -a urls_file="$RUNS_DIR/llm_classifier_false.jsonl" -a truncate_text=1000 -o "$output_file_false"
+
+    echo
+    echo
+    echo
+
     cd - > /dev/null # Go back to the previous directory
 done
+
+echo "Processed $PROCESSED_FILES."
+printf "%s\\n" "${PROCESSED_FILES_NAMES[@]}"
