ad695a6ff554f27b9d808fd31ee53d78c8a30186
Guilherme Garcia <gui.garcia67@gmail.com>
2026-03-13 14:47:26 -0300

    backend: format db file

--- backend/db.py
+++ backend/db.py
@@ -3,15 +3,16 @@ import sqlite3
 import os
 from datetime import datetime, timedelta
 
-DATABASE = os.environ['DATABASE']
+DATABASE = os.environ["DATABASE"]
 
 
 def get_db():
-    db = getattr(g, '_database', None)
+    db = getattr(g, "_database", None)
     if db is None:
         db = g._database = sqlite3.connect(DATABASE)
         db.row_factory = sqlite3.Row
     return db
+
 
 def query_db(query, args=(), one=False):
     cur = get_db().execute(query, args)
@@ -19,17 +20,37 @@ def query_db(query, args=(), one=False):
     cur.close()
     return (rv[0] if rv else None) if one else rv
 
+
 def get_feed_by_domain(domain):
-    return query_db("SELECT f.id as id, domain, feed_url, status_id, fs.name as feed_status, created_at FROM feeds f INNER JOIN feed_status fs ON f.status_id = fs.id WHERE domain = ?", args=[domain], one=True)
+    return query_db(
+        "SELECT f.id as id, domain, feed_url, status_id, fs.name as feed_status, created_at FROM feeds f INNER JOIN feed_status fs ON f.status_id = fs.id WHERE domain = ?",
+        args=[domain],
+        one=True,
+    )
+
 
 def get_feed_by_url(feed_url):
-    return query_db("SELECT f.id as id, domain, feed_url, fs.name as feed_status, created_at FROM feeds f INNER JOIN feed_status fs ON f.status_id = fs.id WHERE f.feed_url = ?", [feed_url], one=True)
+    return query_db(
+        "SELECT f.id as id, domain, feed_url, fs.name as feed_status, created_at FROM feeds f INNER JOIN feed_status fs ON f.status_id = fs.id WHERE f.feed_url = ?",
+        [feed_url],
+        one=True,
+    )
+
 
 def get_feed_by_id(feed_id):
-    return query_db("SELECT f.id as id, domain, feed_url, fs.name as feed_status, created_at FROM feeds f INNER JOIN feed_status fs ON f.status_id = fs.id WHERE f.id = ?", [feed_id], one=True)
+    return query_db(
+        "SELECT f.id as id, domain, feed_url, fs.name as feed_status, created_at FROM feeds f INNER JOIN feed_status fs ON f.status_id = fs.id WHERE f.id = ?",
+        [feed_id],
+        one=True,
+    )
+
 
 def get_oldest_crawled_feed():
-    return query_db("SELECT f.id as id, domain, feed_url, fs.name as feed_status, created_at FROM feeds f INNER JOIN feed_status fs ON f.status_id = fs.id WHERE f.status_id = 2 ORDER BY created_at ASC", one=True)
+    return query_db(
+        "SELECT f.id as id, domain, feed_url, fs.name as feed_status, created_at FROM feeds f INNER JOIN feed_status fs ON f.status_id = fs.id WHERE f.status_id = 2 ORDER BY created_at ASC",
+        one=True,
+    )
+
 
 def get_stalest_feeds(limit, status_ids):
     status_ids = list(status_ids)
@@ -48,56 +69,88 @@ def get_stalest_feeds(limit, status_ids):
         params.append(limit)
     return query_db(query, params)
 
+
 def get_blocked_feeds_description():
-    return query_db("SELECT f.domain, fs.name, fsh1.descr FROM feeds f INNER JOIN feed_status fs ON f.status_id = fs.id LEFT JOIN feed_status_history fsh1 ON (f.id = fsh1.feed_id ) LEFT OUTER JOIN feed_status_history fsh2 ON (f.id = fsh2.feed_id AND fsh1.created_at < fsh2.created_at) WHERE f.status_id = 4 ORDER BY fsh1.created_at DESC")
+    return query_db(
+        "SELECT f.domain, fs.name, fsh1.descr FROM feeds f INNER JOIN feed_status fs ON f.status_id = fs.id LEFT JOIN feed_status_history fsh1 ON (f.id = fsh1.feed_id ) LEFT OUTER JOIN feed_status_history fsh2 ON (f.id = fsh2.feed_id AND fsh1.created_at < fsh2.created_at) WHERE f.status_id = 4 ORDER BY fsh1.created_at DESC"
+    )
+
 
 def get_feeds():
     return query_db("SELECT domain FROM feeds")
+
 
 def update_feed_status(feed_id, new_status):
     con = get_db()
     con.execute("UPDATE feeds SET status_id = ? WHERE id = ?", [new_status, feed_id])
     con.commit()
 
+
 def insert_feed(domain, feed_url, status_id):
     con = get_db()
-    con.execute("INSERT INTO feeds (domain, feed_url, status_id) VALUES (?, ?, ?)", [domain, feed_url, status_id])
+    con.execute(
+        "INSERT INTO feeds (domain, feed_url, status_id) VALUES (?, ?, ?)",
+        [domain, feed_url, status_id],
+    )
     con.commit()
+
 
 def batch_update_crawled_at(feed_ids):
     con = get_db()
     con.executemany("INSERT INTO feed_crawls (feed_id) VALUES (?)", feed_ids)
     con.commit()
 
+
 def get_report(feed_id, hash_id):
-    return query_db("SELECT hash_id FROM reports WHERE feed_id = ? AND hash_id = ?", args=[feed_id, hash_id], one=True)
+    return query_db(
+        "SELECT hash_id FROM reports WHERE feed_id = ? AND hash_id = ?",
+        args=[feed_id, hash_id],
+        one=True,
+    )
+
 
 def insert_report(feed_id, hash_id):
     con = get_db()
-    con.execute("INSERT INTO reports (feed_id, hash_id) VALUES (?, ?)", [feed_id, hash_id])
+    con.execute(
+        "INSERT INTO reports (feed_id, hash_id) VALUES (?, ?)", [feed_id, hash_id]
+    )
     con.commit()
+
 
 def delete_report(feed_id, hash_id):
     con = get_db()
-    con.execute("DELETE FROM reports WHERE feed_id = ? AND hash_id = ?", [feed_id, hash_id])
+    con.execute(
+        "DELETE FROM reports WHERE feed_id = ? AND hash_id = ?", [feed_id, hash_id]
+    )
     con.commit()
+
 
 def insert_feed_history(feed_id, status_id, desc):
     con = get_db()
-    con.execute("INSERT INTO feed_status_history (feed_id, status_id, descr) VALUES (?, ?, ?)", [feed_id, status_id, desc])
+    con.execute(
+        "INSERT INTO feed_status_history (feed_id, status_id, descr) VALUES (?, ?, ?)",
+        [feed_id, status_id, desc],
+    )
     con.commit()
+
 
 def get_blocklist():
     return query_db("SELECT domain FROM blocklist")
+
 
 def add_to_blocklist(domain):
     con = get_db()
     con.execute("INSERT INTO blocklist (domain) VALUES (?)", [domain])
     con.commit()
 
+
 def get_feeds_for_processing(num_feeds, last_process_interval):
     cutoff = datetime.utcnow() - timedelta(minutes=last_process_interval)
-    return query_db("SELECT * FROM feeds WHERE (last_checked_at IS NULL OR last_checked_at <= ?) ORDER BY COALESCE(last_checked_at, '1970-01-01 00:00:00') AND status_id IN (1, 2) ASC LIMIT ?", [cutoff, num_feeds])
+    return query_db(
+        "SELECT * FROM feeds WHERE (last_checked_at IS NULL OR last_checked_at <= ?) ORDER BY COALESCE(last_checked_at, '1970-01-01 00:00:00') AND status_id IN (1, 2) ASC LIMIT ?",
+        [cutoff, num_feeds],
+    )
+
 
 def insert_feed_item(feed_id, feed_item):
     entry_date = datetime(*feed_item.published_parsed[:6]).isoformat()
@@ -107,19 +160,28 @@ def insert_feed_item(feed_id, feed_item):
     con.execute("INSERT INTO feed_items (feed_id, title, url, guid, published_at) VALUES (?, ?, ?, ?, ?)", [feed_id, feed_item.title, feed_item.link, entry_guid, entry_date])
     con.commit()
 
+
 def mark_feed_checked(feed_id):
     con = get_db()
-    con.execute("UPDATE feeds SET last_checked_at = DATETIME('now') WHERE id = ?", [feed_id])
+    con.execute(
+        "UPDATE feeds SET last_checked_at = DATETIME('now') WHERE id = ?", [feed_id]
+    )
     con.commit()
 
+
 def get_id_from_guid(feed_id, feed_item_guid):
-    res = query_db("SELECT id FROM feed_items WHERE feed_id = ? AND guid = ? ORDER BY id DESC LIMIT 1",
-                args=[feed_id, feed_item_guid],
-                one=True
-            )
-    return res['id']
+    res = query_db(
+        "SELECT id FROM feed_items WHERE feed_id = ? AND guid = ? ORDER BY id DESC LIMIT 1",
+        args=[feed_id, feed_item_guid],
+        one=True,
+    )
+    return res["id"]
+
 
 def update_feed_latest(feed_id, last_post_guid, last_feed_item_id):
     con = get_db()
-    con.execute("UPDATE feeds SET last_feed_item_id = ?, last_post_guid = ? WHERE id = ?", [last_feed_item_id, last_post_guid, feed_id])
+    con.execute(
+        "UPDATE feeds SET last_feed_item_id = ?, last_post_guid = ? WHERE id = ?",
+        [last_feed_item_id, last_post_guid, feed_id],
+    )
     con.commit()
