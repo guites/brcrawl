7bbad12fcb43d33fbbf0065006d8f2d38a9af945
Guilherme Garcia <gui.garcia67@gmail.com>
2026-03-13 14:47:44 -0300

    backend: format functions file

--- backend/functions.py
+++ backend/functions.py
@@ -3,20 +3,21 @@ from datetime import datetime
 import os
 
 # TODO: current implementation is closer to a pepper
-SALT = os.getenv('SALT')
+SALT = os.getenv("SALT")
 
 """adapted from https://github.com/HermanMartinus/bearblog/blob/master/blogs/helpers.py#L121"""
-def salt_and_hash(request, duration='day'):
+
+
+def salt_and_hash(request, duration="day"):
     ip = client_ip(request)
-    if duration == 'year':
+    if duration == "year":
         ip_date_salt_string = f"{ip}-{datetime.now().year}-{SALT}"
     else:
         ip_date_salt_string = f"{ip}-{datetime.now().date()}-{SALT}"
 
-    hash_id = hashlib.sha256(
-        ip_date_salt_string.encode('utf-8')
-    ).digest()
+    hash_id = hashlib.sha256(ip_date_salt_string.encode("utf-8")).digest()
     return hash_id
+
 
 def client_ip(request):
     return request.remote_addr
